d04a7b56d5c9ee681bc381751fc95166a4754587
iamm28 <alisonmackay246@gmail.com>
2018-01-15 12:43:06 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for(let i=0; i < arr.length; i++) {
+    // console.log(arr[i])
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`)
+  }
+  return arr
+}
+
+function tailsNeverFails() {
+  let count = 0
+  while (Math.random() >= .5) {
+    count++
+  }
+  return `You got ${count} tails in a row!`
+}
+
+// function tOrf() {
+//   if () {
+//     true
+//   } else {
+//     false
+//   }
+// }
